d346401ffe5b3ace42e49ddc2474a51179849cd6
Ashkaan Jaberi <ashkaan14@gmail.com>
2017-09-30 11:53:13 -0700

    reworked file structure

---
+++ index.js
@@ -0,0 +1,19 @@
+// file-worker module works on reading and then writing a file
+// transofrmer file just works on using each of my transformers/
+//index is the controller.  it'll read file-worker . go to transform. then back to file-worker to write clear
+
+'use strict';
+
+const file = require('./lib/file-worker.js');
+const transform = require('./lib/transformer.js');
+
+let transformBitmap = function(inputFile, outputFile, transform) {
+  let dataBuffer = file.read(inputFile);
+  console.log(dataBuffer);
+  // transform(dataBuffer);
+};
+
+// file.read('./_test_/asset/bitmap.bmp');
+// file.read('pooop');
+ 
+transformBitmap('./_test_/asset/bitmap.bmp', null, null);
