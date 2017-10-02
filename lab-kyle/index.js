ece4ef4a666acf4109a6c03bd54d83d6d5a35426
Kyle Richey <richey.kyle.i@gmail.com>
2017-10-01 18:25:14 -0700

    comments in index, added log in newBitmap

--- lab-kyle/index.js
+++ lab-kyle/index.js
@@ -4,7 +4,16 @@ const readFile = require('./lib/read-file.js');
 const newBitmap = require('./lib/new-bitmap.js');
 const colorShift = require('./lib/color-shift.js');
 
+//Module Syntax:
+//readFile(oldImg)
+
+//colorShift(buffer, palStart, palEnd, removeIndex)
+
+//newBitmap(transformFunction, newImgUrl)
+
 
 newBitmap(colorShift.shift1(readFile('../asset/bitmap.bmp'), 54, 1077, 54), '../asset/colorshift-bitmap.bmp');
 
 newBitmap(colorShift.shift1(readFile('../asset/colorshift-bitmap.bmp'), 54, 1077, 54), '../asset/colorshift-again-bitmap.bmp');
+
+newBitmap(colorShift.shift1(readFile('../asset/finger-print.bmp'), 54, 1077, 54), '../asset/colorshift-finger-print.bmp');
--- lab-kyle/lib/new-bitmap.js
+++ lab-kyle/lib/new-bitmap.js
@@ -4,8 +4,9 @@ const writeFile = require('../lib/write-file.js');
 
 const newBitmap = module.exports = function(transformFunction, newImgUrl){
 
-  // readFile(oldImg);
   let transform = transformFunction;
   writeFile(newImgUrl, transform);
+
+  console.log(newImgUrl);
 
 };
