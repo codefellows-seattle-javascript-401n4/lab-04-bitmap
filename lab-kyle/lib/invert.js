04a1055bf1e01dcbaa7fc9d71cc44fde4629f39c
Kyle Richey <richey.kyle.i@gmail.com>
2017-10-02 20:45:42 -0700

    I believe I have got the invert module working

--- lab-kyle/lib/invert.js
+++ lab-kyle/lib/invert.js
@@ -6,14 +6,9 @@ const invert = module.exports = function (buffer, palStart, palEnd){
   let pallete = buffer.slice(palStart, palEnd + 1);
   let image = buffer.slice(palEnd + 1);
 
-  // console.log(header.length);
-  // console.log(pallete.length);
-
-  for(let i = 0; i <= pallete.length; i++){
-    pallete[i] = 256 - i;
-
+  for(let i = 0; i <= pallete.length; i += 4){
+    pallete[i] = 255 - 54 + i * 4;
   }
-  console.log(pallete);
 
   let list = [header, pallete, image];
   let newBuffer = Buffer.concat(list);
