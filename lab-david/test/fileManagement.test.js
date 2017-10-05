f609380295ce0378e01af0f03eabb0b43879c28d
David Boucher <david.boucher@ookla.com>
2017-10-04 17:56:09 -0700

    Reworked lab 4 and included tests

--- lab-david/test/fileManagement.test.js
+++ lab-david/test/fileManagement.test.js
@@ -1,33 +1,31 @@
 // 'use strict';
 
-// const pingUser = require('../lib/fileManagement.js');
-// const expect = require('expect');
+const fileLoader = require('../lib/fileManagement.js');
+const expect = require('expect');
 
-// describe('File Management Success', () => {
-//     let loadedFile = '';
-//     before(function(done){
-//         pingUser.loadFile('bitmap').then(file => {
-//             loadedFile = file;
-//             done();
-//         }); 
-//     });
+describe('File Management Success', () => {
+    it('should correctly load a file from file system', () => {
+        fileLoader.loadFile('bitmap').then(file => expect(typeof file).toEqual('object'));
+    });
 
-//     it('should return', (done) => {
-//         let typeOf = typeof loadedFile;
-//         expect(typeOf).toEqual('object');
-//     });
-// });
+    it('should save file correctly if a file is saved and responsd with "File Saved!"', () => {
+        fileLoader.loadFile('bitmap').then(file => {
+            fileLoader.saveFile(file, 'newFile').then(response => expect(response).toEqual('File Saved!'));
+        });
+    });
 
-// describe('File Management Failure', () => {
-//     let loadedFile = '';
-//     before(function(done){
-//         pingUser.loadFile().then(file => {
-//             loadedFile = file;
-//             done();
-//         }); 
-//     });
+});
 
-//     it('should return', (done) => {
-//         console.log(loadedFile);
-//     });
-// });
+describe('File Management Failure', () => {
+
+    it('Should throw error message when a file is not loaded', () => {
+        fileLoader.loadFile(null).catch(err => expect(err).toEqual('Did not manage to load a file'));
+    });
+
+    it('should say we could not load the file because we did not pass in a file to save', () => {
+        fileLoader.loadFile('bitmap').then(file => {
+            fileLoader.saveFile(null, 'newFile').catch(err => expect(err).toEqual('Did not manage to save a file'));
+        });
+    });
+
+});
